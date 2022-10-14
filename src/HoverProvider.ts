43bd409fa6d18e86c01e9fb9baff87ad245a85d1
Sandro <s.wu1@studenti.unipi.it>
2022-10-14 19:09:07 +0200

    Revert "add reti"
    This reverts commit 7ca78bb1596f1b32e155c4653e2800f96f2356d4.

--- src/HoverProvider.ts
+++ src/HoverProvider.ts
@@ -7,22 +7,6 @@ import {Config} from './config';
 import {readCommentsForLine} from './comments';
 
 
-
-// ---------------Custom Code -------------------
-let docs = {}
-try {
-    const data = fs.readFileSync(__dirname + '/doc.s').toString();
-    // hoverTexts.push(new vscode.MarkdownString(data))
-    const docsArray = data.split('\n\n\n').map((el: string) => el.split('\n'))
-    docsArray.forEach((docArr : string[]) => {
-        // let flags = docArr.pop();
-        let [name, command, ...info] = docArr;
-        name = name.replace(' ', '');
-        docs[name] = { command, info }
-    })
-} catch { (e: any) => console.log(e); }
-// ----------------------------------------------
-
 /**
  * HoverProvider for assembly language.
  */
